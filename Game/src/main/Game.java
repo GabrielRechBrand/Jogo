f9ec5bb948fe0f22d18c493309e60a6c0a17bab1
GabrielRechBrand <69530575+GabrielRechBrand@users.noreply.github.com>
2020-08-19 13:31:06 -0300

    Update 1

--- Game/src/main/Game.java
+++ Game/src/main/Game.java
@@ -10,56 +10,76 @@ public class Game {
         int vidaInimigo = 100;
 
         System.out.println("Bem-vindo ao jogo!");
-        System.out.println("");
+        System.out.println("Guerreiros têm mais dano, e Magos possuem mais cura, qual você escolhe?");
+
+        String escolhaClasse = JOptionPane.showInputDialog("Guerreiro (1) Mago (2)");
+        int numeroClasse = Integer.parseInt(escolhaClasse);
+        for (int i = 0; i < 7; ++i)
+            System.out.println();
+
         System.out.println("Sua vida = " + vidaHeroi);
         System.out.println("Vida do seu inimigo = " + vidaInimigo);
         System.out.println("");
 
+        for (int i = 0; i < 7; ++i)
+            System.out.println();
+
         Random random = new Random();
 
-            while (vidaHeroi > 0 || vidaInimigo > 0) {
+        while (vidaHeroi > 0 || vidaInimigo > 0) {
 
-                String escolhaAcao = JOptionPane.showInputDialog("Escolha 1 para atacar ou 2 para curar");
-                int numeroAcao = Integer.parseInt(escolhaAcao);
+            String escolhaAcao = JOptionPane.showInputDialog("Escolha 1 para atacar ou 2 para curar");
+            int numeroAcao = Integer.parseInt(escolhaAcao);
+            int escolhaBot = random.nextInt(2);
 
-                int escolhaBot = random.nextInt(2);
+            if (escolhaBot == 0) {
+                int geraDano = random.nextInt(11);
+                vidaHeroi = vidaHeroi - geraDano;
+                System.out.println("O inimigo atacou");
+            } else if (escolhaBot == 1) {
+                int geraCura = random.nextInt(11);
+                vidaInimigo = vidaInimigo + geraCura;
+                System.out.println("O inimigo curou");
+            } else if (escolhaBot == 2) {
+                System.out.println("O inimigo errou o golpe");
+            }
 
-                if (escolhaBot == 0) {
-                    System.out.println("O inimigo atacou");
-                    vidaHeroi = vidaHeroi - 10;
-                } else if (escolhaBot == 1) {
-                    System.out.println("O inimigo curou");
-                    vidaInimigo = vidaInimigo + 10;
-                }
-
-                if (vidaInimigo == 0) {
-                    System.out.println("Você venceu!");
-                    break;
-                } else if (vidaHeroi == 0) {
-                    System.out.println("Você perdeu!");
-                    break;
-                }
-
-                if (numeroAcao == 1) {
-                    vidaInimigo = vidaInimigo - 10;
+            if (vidaInimigo <= 0) {
+                System.out.println("Você venceu!");
+                break;
+            } else if (vidaHeroi <= 0) {
+                System.out.println("Você perdeu!");
+                break;
+            }
+            if (numeroAcao == 1) {
+                    int geraDano = random.nextInt(16);
+                    vidaInimigo = vidaInimigo - geraDano;
                     System.out.println("Você atacou");
                     System.out.println("Sua vida = " + vidaHeroi);
                     System.out.println("Vida do seu inimigo = " + vidaInimigo);
                     System.out.println("");
-                } else if (numeroAcao == 2) {
-                    vidaHeroi = vidaHeroi + 10;
+                    for (int i = 0; i < 7; ++i)
+                        System.out.println();
+            } else if (numeroAcao == 2) {
+                    int geraCura = random.nextInt(11);
+                    vidaInimigo = vidaInimigo - geraCura;
                     System.out.println("Você usou a cura");
                     System.out.println("Sua vida = " + vidaHeroi);
                     System.out.println("Vida do seu inimigo = " + vidaInimigo);
                     System.out.println("");
-                } else if (numeroAcao != 1 || numeroAcao != 2) {
+                    for (int i = 0; i < 7; ++i)
+                        System.out.println();
+            } else if (numeroAcao != 1 || numeroAcao != 2) {
                     System.out.println("Você não executou nenhuma ação...");
                     System.out.println("");
                     System.out.println("Sua vida = " + vidaHeroi);
                     System.out.println("Vida do seu inimigo = " + vidaInimigo);
                     System.out.println("");
-                }
+                    for (int i = 0; i < 71; ++i)
+                        System.out.println();
+
             }
+        }
     }
 }
 
