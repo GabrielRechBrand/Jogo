659f45420cf9ba43b58525ffd488b2c3b454f2ee
GabrielRechBrand <69530575+GabrielRechBrand@users.noreply.github.com>
2020-08-18 22:08:29 -0300

    Add files via upload

---
+++ Game/src/main/Game.java
@@ -0,0 +1,26 @@
+package main;
+
+import javax.swing.*;
+
+public class Game {
+    public static void main(String[] args) {
+
+        int vidaHeroi = 10;
+        int vidaHeroiBuff;
+        int vidaInimigo = 10;
+
+        System.out.println("Bem-vindo ao jogo!");
+        System.out.println("Sua vida = " + vidaHeroi);
+        System.out.println("Vida do seu inimigo = " + vidaInimigo);
+        System.out.println("");
+
+        boolean buffVida;
+
+        for (buffVida=true; (vidaHeroi >= 1 && vidaHeroi <= 98);) {
+            vidaHeroi = vidaHeroi + 2;
+
+        }
+        System.out.println("Sua vida = " + vidaHeroi);
+        System.out.println("Vida do seu inimigo = " + vidaInimigo);
+    }
+}
